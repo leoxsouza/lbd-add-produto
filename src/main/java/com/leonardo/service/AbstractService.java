eb3c1ed5838dc74f3cefc4e816504267925ce464
Leonardo Oliveira <leoxsouza286@gmail.com>
2025-01-01 17:27:53 -0300

    Refactor constants usage in AbstractService.
    Replaced hardcoded strings with class constants for better maintainability and consistency. Improved readability and reduced duplication by using standard naming conventions for static final constants. No functional changes introduced.

--- src/main/java/com/leonardo/service/AbstractService.java
+++ src/main/java/com/leonardo/service/AbstractService.java
@@ -1,23 +1,20 @@
 package com.leonardo.service;
 
-import com.leonardo.dto.in.Item;
 import com.leonardo.dto.in.Order;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.ScanRequest;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public abstract class AbstractService {
 
-    public final static String ORDER_PEDIDOID_COL = "pedidoId";
-    public final static String ORDER_ITEMS_COL = "items";
-    public final static String ORDER_STATUS_COL = "status";
-    public final static String ORDER_TOTAL_COL = "total";
-    public final static String ORDER_TIMESTAMP_COL = "timestamp";
-    public final static String ORDER_OBSERVACAO_COL = "observacao";
+    public static final String ORDER_PEDIDOID_COL = "pedidoId";
+    public static final String ORDER_ITEMS_COL = "items";
+    public static final String ORDER_STATUS_COL = "status";
+    public static final String ORDER_TOTAL_COL = "total";
+    public static final String ORDER_TIMESTAMP_COL = "timestamp";
+    public static final String ORDER_OBSERVACAO_COL = "observacao";
 
     public String getTableName() {
         return "Orders";
@@ -36,12 +33,12 @@ public abstract class AbstractService {
 
     private Map<String, AttributeValue> getItem(Order order) {
         return Map.of(
-                "pedidoId", AttributeValue.builder().s(order.pedidoId()).build(),
+                ORDER_PEDIDOID_COL, AttributeValue.builder().s(order.pedidoId()).build(),
 //                "items", AttributeValue.builder().l(order.items().stream().map(this::getItem).collect(Collectors.toList())).build(), TODO: understand how to map this items
-                "status", AttributeValue.builder().s(order.status()).build(),
-                "total", AttributeValue.builder().n(order.total().toString()).build(),
-                "timestamp", AttributeValue.builder().s(order.timestamp()).build(),
-                "observacao", AttributeValue.builder().s(order.observacao()).build()
+                ORDER_STATUS_COL, AttributeValue.builder().s(order.status()).build(),
+                ORDER_TOTAL_COL, AttributeValue.builder().n(order.total().toString()).build(),
+                ORDER_TIMESTAMP_COL, AttributeValue.builder().s(order.timestamp()).build(),
+                ORDER_OBSERVACAO_COL, AttributeValue.builder().s(order.observacao()).build()
         );
     }
 
